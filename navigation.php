c72bf0f227e99e1f082bf00e2c554ebf5d6bde40
root <root@DESKTOP-TABJ5LE.localdomain>
2022-04-20 20:36:20 +0200

    hotfix

--- navigation.php
+++ navigation.php
@@ -6,5 +6,14 @@ return [
         'children' => [
         ],
     ],
+    'Environment Setups' => [
+        'url' => 'docs/getting-started',
+        'children' => [
+            'Windows (Homestead)' => 'docs/windows-homestead',
+            'Windows (Docker)' => 'docs/windows-docker',
+            'MacOS (Valet)' => 'docs/mac-valet',
+            'MacOS (Docker)' => 'docs/mac-docker'
+        ],
+    ],
     'Docs for the Docs' => 'https://jigsaw.tighten.co/docs/installation',
 ];
