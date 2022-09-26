febb20a4ea32ec39ca4404e59d08e8e134b11464
Sergej Maisinger <98972777+msngr@users.noreply.github.com>
2022-09-26 11:59:30 +0000

    add design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,48 @@ module.exports = {
     },
     theme: {
         extend: {
+            boxShadow: {
+                lg: '0 -1px 27px 0 rgba(0, 0, 0, 0.04), 0 4px 15px 0 rgba(0, 0, 0, 0.08)',
+            },
+            colors: {
+                orange: colors.orange,
+                white: '#FFFEFD',
+                primary: {
+                    // blue
+                    // 100: '#F0F4F8',
+                    // 200: '#D9E2EC',
+                    // 300: '#BCCCDC',
+                    400: '#9FB3C8',
+                    500: '#829AB1',
+                    600: '#486581',
+                    700: '#334E68',
+                    800: '#243B53',
+                    900: '#01182F',
+                },
+                secondary: {
+                    // orange
+                    // 100: '#FFE8D9',
+                    // 200: '#FFD0B5',
+                    // 300: '#FFB088',
+                    // 400: '#FF9466',
+                    // 500: '#F9703E',
+                    900: '#F35627',
+                },
+            },
             fontFamily: {
                 sans: ['Gilroy', 'sans-serif'],
                 mono: ['monospace'],
+            },
+            fontSize: {
+                '2xl': '1.5rem',
+                '3xl': '1.75rem',
+                '4xl': '2.125rem',
+                '5xl': '2.625rem',
+                '6xl': '10rem',
+            },
+            letterSpacing: {
+                wide: '0.02em',
+                wider: '0.03em',
             },
             lineHeight: {
                 normal: '1.6',
@@ -26,30 +65,12 @@ module.exports = {
             maxWidth: {
                 none: 'none',
                 '7xl': '80rem',
-                '8xl': '88rem',
+                '8xl': '74.5rem', //  1192px
             },
             spacing: {
                 7: '1.75rem',
                 9: '2.25rem',
             },
-            boxShadow: {
-                lg: '0 -1px 27px 0 rgba(0, 0, 0, 0.04), 0 4px 15px 0 rgba(0, 0, 0, 0.08)',
-            },
-            colors: {
-                orange: colors.orange,
-            },
-        },
-        fontSize: {
-            xs: '.8rem',
-            sm: '.925rem',
-            base: '1rem',
-            lg: '1.125rem',
-            xl: '1.25rem',
-            '2xl': '1.5rem',
-            '3xl': '1.75rem',
-            '4xl': '2.125rem',
-            '5xl': '2.625rem',
-            '6xl': '10rem',
         },
     },
     variants: {
