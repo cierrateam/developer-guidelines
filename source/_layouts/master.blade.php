731691ad33f70731f55f84b10aa7a3cff21bfa4b
Sergej Maisinger <98972777+msngr@users.noreply.github.com>
2022-09-29 13:23:05 +0000

    put head section in own component

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,54 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="description" content="{{ $page->description ?? $page->siteDescription }}">
-
-    <meta property="og:site_name" content="{{ $page->siteName }}" />
-    <meta property="og:title" content="{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}" />
-    <meta property="og:description" content="{{ $page->description ?? $page->siteDescription }}" />
-    <meta property="og:url" content="{{ $page->getUrl() }}" />
-    <meta property="og:image" content="/assets/img/cierra-dark.png" />
-    <meta property="og:type" content="website" />
-
-    <meta name="twitter:image:alt" content="{{ $page->siteName }}">
-    <meta name="twitter:card" content="summary_large_image">
-
-    @if ($page->docsearchApiKey && $page->docsearchIndexName)
-    <meta name="generator" content="tighten_jigsaw_doc">
-    @endif
-
-    <title>{{ $page->siteName }}{{ $page->title ? ' | ' . $page->title : '' }}</title>
-
-    <link rel="home" href="{{ $page->baseUrl }}">
-    <link rel="icon" type="image/png" href="/favicon.png" />
-
-    @stack('meta')
-
-    @if ($page->production)
-    <!-- Insert analytics code here -->
-    @endif
-
-    <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
-
-    @if ($page->docsearchApiKey && $page->docsearchIndexName)
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />
-    @endif
-</head>
+@include('_layouts.head')
 
 <body class="min-h-screen bg-white overflow-x-hidden">
     <header class="z-30 absolute w-full flex items-center py-6 sm:py-5" role="banner">
         <div class="container mx-auto max-w-8xl px-4 xl:px-0">
             <div class="flex items-center justify-between">
                 <a href="/" title="{{ $page->siteName }} home">
-                    {{-- <img class="h-8 md:h-10 mr-3" src="/assets/img/cierra-dark.png"
-                        alt="{{ $page->siteName }} logo" /> --}}
                     @include('_assets.svg.logo')
                 </a>
-                <a class="text-primary-600 hover:text-secondary-900 transition" href="/" title="cierra.de">
+                <a class="text-primary-600 hover:text-secondary-900 transition tracking-wide text-18" href="/"
+                    title="cierra.de">
                     cierra.de
                 </a>
             </div>
