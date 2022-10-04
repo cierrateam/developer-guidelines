9d2e83d2f3c0a41982fa602c3f9b9d7956fff98d
Sergej Maisinger <98972777+msngr@users.noreply.github.com>
2022-10-04 06:09:33 +0000

    add docs-icon with group-hover

---
+++ source/_assets/svg/docs-icon.blade.php
@@ -0,0 +1,6 @@
+<svg class="inline-flex group-hover:stroke-secondary-900" width="16" height="16" viewBox="0 0 16 16" fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path class="group-hover:stroke-secondary-900 transition"
+        d="M6.33325 3.00358H2.99992C2.07944 3.00358 1.33325 3.74977 1.33325 4.67025V13.0036C1.33325 13.9241 2.07944 14.6702 2.99992 14.6702H11.3333C12.2537 14.6702 12.9999 13.9241 12.9999 13.0036V9.67025M9.66658 1.33691H14.6666M14.6666 1.33691V6.33691M14.6666 1.33691L6.33325 9.67025"
+        stroke="#FFFEFD" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
